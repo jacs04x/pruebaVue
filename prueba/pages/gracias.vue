ad0d514e6f0a6dab00b8ff6dc61730c356b0acad
jcastromej <jcastro@ahorra.io>
2023-04-04 06:09:57 -0600

    tabla

--- prueba/pages/gracias.vue
+++ prueba/pages/gracias.vue
@@ -2,7 +2,27 @@
     <div class="container-fluid">
 
         <tarjeta :id="this.$store.state.formulario.id" :email="this.$store.state.formulario.email" />
-
+        <!--       <div class="table-responsive">
+        <table class="table table-striped">
+          <thead style="background-color: #dedede">
+            <tr>
+              <th scope="col">Cobertura</th>
+              <th scope="col">Suma asegurada</th>
+              <th scope="col">Deducible</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(item, index) in this.$store.state.coberturas"
+              :key="index"
+            >
+              <td>{{ item | showName }}</td>
+              <td>{{ item | showSuma }}</td>
+              <td>{{ item | showDeducible }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div> -->
     </div>
 </template>
 <script>
@@ -15,6 +35,9 @@ export default {
 
     components: {
         tarjeta
+    },
+    filters: {
+
     }
 
 }
